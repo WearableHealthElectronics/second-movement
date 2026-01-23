b2cac8cb65b62f647fef202621a3c66bf3e2d31b
WearableHealthElectronics <ricknosemedicine@gmail.com>
2026-01-23 00:08:42 -0500

    Update hello_face.h

--- watch-faces/hello_face.h
+++ watch-faces/hello_face.h
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include "movement.h"
 
+extern const watch_face_t hello_face;
+
 void hello_face_setup(uint8_t watch_face_index, void ** context_ptr);
 void hello_face_activate(void *context);
 bool hello_face_loop(movement_event_t event, void *context);
