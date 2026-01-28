f7d4ca8ed26e28498a3342883eb3940e830cb709
WearableHealthElectronics <ricknosemedicine@gmail.com>
2026-01-28 13:35:27 -0500

    Update movement_config.h

--- movement_config.h
+++ movement_config.h
@@ -31,10 +31,7 @@
 const watch_face_t watch_faces[] = {
     clock_face,
     voltage_face,
-    
-    //world_clock_face,
-    //sunrise_sunset_face,
-    //moon_phase_face,
+    activity_logging_face,
     fast_stopwatch_face,
     //countdown_face,
     alarm_face,
