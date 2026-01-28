4c282a21ab16ff630aed1abbf018236d718d85c2
WearableHealthElectronics <ricknosemedicine@gmail.com>
2026-01-28 13:41:49 -0500

    Update movement_config.h

--- movement_config.h
+++ movement_config.h
@@ -33,6 +33,8 @@ const watch_face_t watch_faces[] = {
     voltage_face,
     activity_logging_face,
     fast_stopwatch_face,
+    ping_face,
+    simple_coin_flip_face,
     //countdown_face,
     alarm_face,
     set_time_face,
@@ -50,7 +52,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 4)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 5)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
